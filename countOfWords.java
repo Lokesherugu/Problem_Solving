41ec99a89b764c69d49442df339606a508818f67
Lokesh Erugu <lokesherugu401@gmail.com>
2025-07-22 11:16:24 +0530

    prints frequency of words in a sentence

---
+++ countOfWords.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class countOfWords {
+    public static void wordCountOf(String s){
+        //splitting the words
+        String[] words=s.split(" ");
+                HashMap<String,Integer> fmap=new HashMap<>();
+
+        for(String word:words){
+            //System.out.println(word);
+        
+        fmap.put(word,fmap.getOrDefault(word,0)+1);
+        }
+        
+        for(String word:fmap.keySet()){
+            System.out.println(word+": "+fmap.get(word));
+        }
+    
+    
+    }
+    public static void main(String[] args) {
+        String sentence="hey...! my name is lokesh";
+        wordCountOf(sentence);
+    }
+    
+}
