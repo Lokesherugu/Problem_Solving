bf71a0db048afe29a0babeab39e37a552768dc23
Lokesh Erugu <lokesherugu401@gmail.com>
2025-07-22 09:39:13 +0530

    diagonal and anti-diagonal of a matrix

---
+++ Matrix_3_3_diagonals.java
@@ -0,0 +1,22 @@
+public class Matrix_3_3_diagonals {
+   public static void diagonalsOf(int[][] arr){
+    for(int i=0;i<3;i++){
+        System.out.print(arr[i][i]+" ");
+        }
+    System.out.println();
+
+    for(int i=0;i<3;i++){
+        System.out.print(arr[i][2-i]+" ");
+    }
+    
+   }
+   public static void main(String[] args) {
+    int[][] arr1={
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    diagonalsOf(arr1);
+    
+   }
+}
